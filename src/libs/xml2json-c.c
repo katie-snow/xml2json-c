8f28dbbd0032bce05e7b7c44fcce78107ce32cf3
katie-snow <katiej.snow@gmail.com>
2015-02-18 11:36:13 -0700

    Implement empty element and element with pure text content.  Fixed autotool files.

--- src/libs/xml2json-c.c
+++ src/libs/xml2json-c.c
@@ -1,5 +1,43 @@
-
 #include "xml2json-c.h"
 
+/*
+ ============================================================================
+ Name        : xml2json-c.c
+ Author      : Katie Snow
+ Version     :
+ Copyright   : Copyright 2015 Katie Snow
+ Description : XML 2 JSON-C main configuration library
+ ============================================================================
+*/
 
+#include <libxml/tree.h>
+#include <json.h>
 
+void xml2jsonc_convert_elements(xmlNode *anode, json_object *jobj)
+{
+  xmlNode *cur_node = NULL;
+  json_object *cur_jobj = NULL;
+  json_object *cur_jstr = NULL;
+
+  for (cur_node = anode; cur_node; cur_node = cur_node->next)
+  {
+    if (cur_node->type == XML_ELEMENT_NODE)
+    {
+      if (xmlChildElementCount(cur_node) == 0)
+      {
+        /* JSON string object */
+        cur_jstr = json_object_new_string(xmlNodeGetContent(cur_node));
+        cur_jobj = json_object_new_object();
+        json_object_object_add(jobj, cur_node->name, cur_jstr);
+      }
+      else
+      {
+        /* JSON object */
+        cur_jobj = json_object_new_object();
+        json_object_object_add(jobj, cur_node->name, json_object_get(cur_jobj));
+      }
+    }
+
+    xml2jsonc_convert_elements(cur_node->children, cur_jobj);
+  }
+}
--- src/libs/xml2json-c.h
+++ src/libs/xml2json-c.h
@@ -1,4 +1,12 @@
-
+/*
+ ============================================================================
+ Name        : xml2json-c.h
+ Author      : Katie Snow
+ Version     :
+ Copyright   : Copyright 2015 Katie Snow
+ Description : XML 2 JSON-C main configuration library
+ ============================================================================
+*/
 
 #ifndef __XML2JSON_C_H_
 #define __XML2JSON_C_H_
@@ -7,11 +15,15 @@
 extern "C" {
 #endif
 
+typedef struct _xmlNode xmlNode;
+typedef struct json_object json_object;
 
-
-
-
-
+/**
+ * converts from xml to a json-c object
+ *
+ * Parameters: pointers to xmlNode and json_object
+ */
+extern void xml2jsonc_convert_elements (xmlNode *anode, json_object* jobj);
 
 #ifdef __cplusplus
 }
